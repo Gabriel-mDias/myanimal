42473ce86738672f7681dfa43655c5e4aa5ee592
gabriel.dias <gabriel.dias@itixti.com.br>
2024-03-30 01:01:43 -0300

    FEAT: Base Project

---
+++ src/main/java/com/gems/myanimal/MyAnimalApplication.java
@@ -0,0 +1,13 @@
+package com.gems.myanimal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAnimalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAnimalApplication.class, args);
+	}
+
+}
